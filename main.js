501d99745ea27983311d11cd7c55cad0f460fd5f
Shrief Essam <shriefessam1999@gmail.com>
2022-06-30 22:30:22 +0200

    add the basic layout and the overlay form

--- main.js
+++ main.js
@@ -22,3 +22,33 @@ function displayBooks(){
     myLibrary.map( book=> console.table(book));
 }
 
+
+const overlayForm = document.querySelector('#overlayForm')
+const addButton = document.querySelector('#addButton');
+const submitButton = document.querySelector('#submitButton');
+
+
+
+
+addButton.addEventListener('click',()=>{
+    overlayForm.style.display = "flex";
+})
+
+
+submitButton.addEventListener('click',()=>{
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let numberOfPages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+
+    const newBook = new Book(title,author,numberOfPages,read);
+    addBookToLibrary(newBook);
+    console.table(myLibrary);
+
+})
+
+
+
+
+
+
