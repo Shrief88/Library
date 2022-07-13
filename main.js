64d41d85ecd63510a067efd9314cca2107a5c3b9
Shrief Essam <shriefessam1999@gmail.com>
2022-07-13 22:08:28 +0200

    use class instead of constructor also use module design

--- main.js
+++ main.js
@@ -1,80 +1,17 @@
-let myLibrary = [];
-const overlayForm = document.querySelector('#overlayForm')
-const addButton = document.querySelector('#addButton');
-const submitButton = document.querySelector('#submitButton');
-const form = document.querySelector('#form');
-const content = document.querySelector('.content');
-
-
-if(localStorage.getItem('books')){
-    myLibrary=JSON.parse(localStorage.getItem('books'));
+class Book{
+    constructor(title, author, numberOfPages, read) {
+        this.id = Date.now();
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.read = read;
+    }
 }
 
-displayAllBooks(myLibrary);
-
-addButton.addEventListener('click',()=>{
-    overlayForm.style.display = "flex";
-})
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-
-    let title = document.querySelector('#title').value;
-    let author = document.querySelector('#author').value;
-    let numberOfPages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').checked;
-
-    const newBook = new Book(title,author,numberOfPages,read);
-    addBookToLibrary(newBook);
-    setData(myLibrary);
-
-    overlayForm.style.display = "none";
-    createBookCard(newBook);
-    document.querySelector('#title').value ='';
-    document.querySelector('#author').value = '';
-    document.querySelector('#pages').value = '';
-})
-
-
-
-content.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('del')){
-        deleteElementFromLocal(e.target.parentElement.parentElement.getAttribute('data-id'));
-        e.target.parentElement.parentElement.remove();
+function displayAllBooks(myLibrary){
+    for (const book of myLibrary){
+       createBookCard(book);
     }
-})
-
-content.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('read')){
-        e.target.classList.toggle('activeButton');
-        const index = e.target.parentElement.parentElement.getAttribute('data-id');
-        myLibrary.find((book)=>{
-           if(book.id == index){
-                book.read =  (book.read === true) ? false:true;
-                setData(myLibrary);
-           }
-        })
-    }
-})
-
-overlayForm.addEventListener('click',(e)=>{
-    if(e.target.getAttribute('id') === 'overlayForm'){
-        overlayForm.style.display = "none";
-    };
-   
-})
-
-
-function Book(title,author,numberOfPages,read){
-    this.id = Date.now();
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-}
-
-function addBookToLibrary(Book) {
-    myLibrary.push(Book);
 }
 
 function createBookCard(book){
@@ -118,20 +55,106 @@ function createBookCard(book){
     content.append(card);
 }
 
-function displayAllBooks(myLibrary){
-    for (const book of myLibrary){
-       createBookCard(book);
+
+const library = (()=>{
+    let myLibrary = [];
+    if(localStorage.getItem('books')){
+        myLibrary=JSON.parse(localStorage.getItem('books'));
     }
-}
 
-function setData(myLibrary){
-    window.localStorage.setItem("books",JSON.stringify(myLibrary));
-}
+    const getLibrary = ()=>{
+        return myLibrary;
+    }
 
-function deleteElementFromLocal(book_id){
-    let myNewLibrary = myLibrary.filter((book)=> book.id != book_id );
-    setData(myNewLibrary);
-}
+    const addBookToLibrary = (Book)=>{
+        myLibrary.push(Book);
+    }
+
+    const setData = (myLibrary)=>{
+
+        window.localStorage.setItem("books",JSON.stringify(myLibrary));
+    }
+
+    const deleteElementFromLocal = (book_id)=>{
+        let myNewLibrary = myLibrary.filter((book)=> book.id != book_id );
+        setData(myNewLibrary);
+        myLibrary = myNewLibrary;
+    }
+
+    return{ addBookToLibrary,setData,deleteElementFromLocal,getLibrary}
+})();
+
+const displayController = (()=>{
+    const overlayForm = document.querySelector('#overlayForm')
+    const addButton = document.querySelector('#addButton');
+    const submitButton = document.querySelector('#submitButton');
+    const form = document.querySelector('#form');
+    const content = document.querySelector('.content');
+
+    const myLibrary = library.getLibrary()
+    displayAllBooks(myLibrary);
+
+    overlayForm.addEventListener('click',(e)=>{
+        if(e.target.getAttribute('id') === 'overlayForm'){
+            overlayForm.style.display = "none";
+        };
+    })
+
+    addButton.addEventListener('click',()=>{
+        overlayForm.style.display = "flex";
+    })
+
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault(); //prevent the from submitting 
+    
+        let title = document.querySelector('#title').value;
+        let author = document.querySelector('#author').value;
+        let numberOfPages = document.querySelector('#pages').value;
+        let read = document.querySelector('#read').checked;
+    
+        const newBook = new Book(title,author,numberOfPages,read);
+        library.addBookToLibrary(newBook);
+        library.setData(myLibrary);
+    
+        overlayForm.style.display = "none";
+
+        createBookCard(newBook);
+
+        document.querySelector('#title').value ='';
+        document.querySelector('#author').value = '';
+        document.querySelector('#pages').value = '';
+    })
+
+    content.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('del')){
+            library.deleteElementFromLocal(e.target.parentElement.parentElement.getAttribute('data-id'));
+            e.target.parentElement.parentElement.remove();
+        }
+    })
+
+    content.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('read')){
+            e.target.classList.toggle('activeButton');
+            const index = e.target.parentElement.parentElement.getAttribute('data-id');
+            myLibrary.find((book)=>{
+               if(book.id == index){
+                    book.read =  (book.read === true) ? false:true;
+                    library.setData(myLibrary);
+               }
+            })
+        }
+    })
+})();
+
+
+
+
+
+
+
+
+
+
 
 
 
